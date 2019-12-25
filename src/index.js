7f5510c9e27e9a2febcfab73eb332a6fedcb8e30
vinayshetty <vinayshetty@users.noreply.github.com>
2019-12-25 17:45:26 +0530

    Helloworld
    Hello world

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+function Helloworld() {
+  return <div> Hello world</div>;
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Helloworld />, document.getElementById("mountNode"));
